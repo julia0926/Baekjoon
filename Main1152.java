129b85455d91656826c188f9822ed5e256e23acc
Julia <kj980926@naver.com>
2021-02-04 12:43:54 +0900

    first commit

---
+++ Main1152.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class Main1152 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String s=scanner.nextLine();
+		StringTokenizer st=new StringTokenizer(s," ");
+	}
+
+}
